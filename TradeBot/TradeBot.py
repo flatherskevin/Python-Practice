07f6e0eb1ce1dbd02f969cdbb9af8334f18a3d2d
flatherskevin <flatherskevin@gmail.com>
2017-08-27 00:18:45 -0400

    added print_pretty function

--- TradeBot/TradeBot.py
+++ TradeBot/TradeBot.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 class TradeBot:
 	def __init__(self, api_version='3'):
@@ -25,14 +26,32 @@ class TradeBot:
 		def history(self):
 			return self.__history
 
+		def print_pretty(self, type='current'):
+			def __print(data):
+				print('\n\n')
+				for item in self.__request:
+					print('***{item}***'.format(item=item))
+					for subitem in self.__request[item]:
+						if subitem in ['buy','sell','high','low','avg','last']:
+							self.__request[item][subitem] = '{0:.10f}'.format(self.__request[item][subitem])
+						elif subitem == 'updated':
+							self.__request[item][subitem] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(self.__request[item][subitem])))
+						print('{key}	:	{value}'.format(key=subitem,value=self.__request[item][subitem]))
+					print('\n\n')
+			if type == 'current':
+				__print(self.__request)
+			elif type == 'history':
+				for item in self.__history:
+					__print(item)
+
 	def Info(self, pair_list):
 		return self.__Info(self, pair_list)
 
 bot = TradeBot()
 
-pair_list = ['btc_usd','hcc_btc','maze_btc']
+pair_list = ['btc_usd','html5_btc','maze_btc']
 
 charlie = bot.Info(pair_list)
 
 charlie.new_request()
-print(charlie.history)
+charlie.print_pretty()

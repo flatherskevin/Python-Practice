2e7e9b020e06bffa04a26c9917a372b804eea7ad
flatherskevin <flatherskevin@gmail.com>
2017-08-27 10:03:37 -0400

    added Info class and began work on chunking url calls

--- TradeBot/TradeBot.py
+++ TradeBot/TradeBot.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import itertools
 
 class TradeBot:
 	def __init__(self, api_version='3'):
@@ -9,49 +10,113 @@ class TradeBot:
 	def get_url(self):
 		return self.__url
 
+	def __grouper(self, n, iterable, fillvalue=''):
+		args = [iter(iterable) * n]
+		return itertools.zip_longest(fillvalue=fillvalue, *args)
+
 	class __Info:
-		def __init__(self, parent, pair_list):
+		def __init__(self, parent):
 			assert list(pair_list)
 			self.__parent = parent
-			self.__url = '{super_url}ticker/{pair}'.format(super_url=parent.get_url(),pair='-'.join(pair_list))
+			self.__url = '{super_url}info'.format(super_url=parent.get_url())
 			print(self.__url)
 			self.__request = None
 			self.__history = []
+			self.__pairs = []
 
 		def new_request(self):
 			self.__request = requests.get(self.__url).json()
 			self.__history.append(self.__request)
-			
-		@property
-		def history(self):
-			return self.__history
+			self.__pairs_to_list()
+
+		def __pairs_to_list(self):
+			for item in self.__request['pairs']:
+				self.__pairs.append(item)
 
 		def print_pretty(self, type='current'):
 			def __print(data):
-				print('\n\n')
-				for item in self.__request:
-					print('***{item}***'.format(item=item))
-					for subitem in self.__request[item]:
-						if subitem in ['buy','sell','high','low','avg','last']:
-							self.__request[item][subitem] = '{0:.10f}'.format(self.__request[item][subitem])
-						elif subitem == 'updated':
-							self.__request[item][subitem] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(self.__request[item][subitem])))
-						print('{key}	:	{value}'.format(key=subitem,value=self.__request[item][subitem]))
-					print('\n\n')
+				print('\n')
+				for item in data['pairs']:
+					print(item)
+
 			if type == 'current':
 				__print(self.__request)
 			elif type == 'history':
 				for item in self.__history:
 					__print(item)
 
-	def Info(self, pair_list):
-		return self.__Info(self, pair_list)
+		@property
+		def pairs(self):
+			return self.__pairs
+
+
+	class __Ticker:
+		def __init__(self, parent, pair_list):
+			assert list(pair_list)
+			self.__parent = parent
+			self.__pair_list = pair_list
+			self.__set_url()
+			print(self.__url)
+			self.__request = None
+			self.__request_json = {}
+			self.__history = []
+
+		def new_request(self):
+			self.__request = requests.get(self.__url)
+			if self.__request.status_code == 200:
+				self.__request_json = self.__request.json()
+			elif self.__request.status_code == 414:
+				for item in self.__parent.__grouper(10, self.__pair_list):
+					self.__set_url()
+					self.__request_json = self.__request_json.append(requests.get(self.__url).json())
+
+			self.__history.append(self.__request)
+			
+		def __set_url(self):
+			self.__url = '{super_url}ticker/{pair}'.format(super_url=self.__parent.get_url(),pair='-'.join(self.__pair_list))
+
+		@property
+		def history(self):
+			return self.__history
+
+		def print_pretty(self, type='current'):
+			def __print(data):
+				print('\n')
+				for item in data:
+					print('***{item}***'.format(item=item))
+					for subitem in data[item]:
+						if subitem in ['buy','sell','high','low','avg','last']:
+							data[item][subitem] = '{0:.10f}'.format(data[item][subitem])
+						elif subitem == 'updated':
+							data[item][subitem] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(data[item][subitem])))
+						print('{key}	:	{value}'.format(key=subitem,value=data[item][subitem]))
+					print('\n')
+			if type == 'current':
+				__print(self.__request_json)
+			elif type == 'history':
+				for item in self.__history:
+					__print(item)
+
+	def Ticker(self, pair_list):
+		return self.__Ticker(self, pair_list)
+
+	def Info(self):
+		return self.__Info(self)
+
+
+
+#######################################################################
 
 bot = TradeBot()
 
 pair_list = ['btc_usd','html5_btc','maze_btc']
 
-charlie = bot.Info(pair_list)
+charlie = bot.Info()
+
 
 charlie.new_request()
-charlie.print_pretty()
+#print(charlie.pairs)
+#jackie = bot.Ticker(pair_list)
+jackie = bot.Ticker(charlie.pairs)
+jackie.new_request()
+jackie.print_pretty()
